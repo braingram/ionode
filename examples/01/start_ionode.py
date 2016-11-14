0a9a4a21735d116e0b19e3dd3eadcf2695e15bde
Brett Graham <brettgraham@gmail.com>
2016-11-14 16:37:53 -0500

    adding auto-populated namespace to ionode and automatic signal connection

--- examples/01/start_ionode.py
+++ examples/01/start_ionode.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
+import pizco
+
 import ionode.base
 
-node = ionode.base.IONode('config.json')
-node.serve_forever()
+
+class MyIONode(ionode.base.IONode):
+    def __init__(self, cfg=None):
+        super(MyIONode, self).__init__(cfg)
+        self.mysignal = pizco.Signal(nargs=1)
+
+    def trigger_signal(self, arg):
+        print "Triggering signal with %s" % (arg, )
+        self.mysignal.emit(arg)
+
+if __name__ == '__main__':
+    node = MyIONode('config.json')
+    node.serve_forever()
--- examples/01/start_ui.py
+++ examples/01/start_ui.py
@@ -6,8 +6,10 @@ import webbrowser
 import ionode.base
 import ionode.ui.base
 
+from start_ionode import MyIONode
+
 url = 'http://127.0.0.1:5000/base'
 config = json.load(open('config.json', 'r'))
 print("Use Ctrl-C in this terminal window to stop the ui and server")
 webbrowser.open(url)
-ionode.ui.base.run_proxy_ui('base', config['addr'], ionode.base.IONode)
+ionode.ui.base.run_proxy_ui('base', config['addr'], MyIONode)
