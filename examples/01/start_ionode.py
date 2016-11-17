90db2fb06dd72d093ecc8bfb8d711a5e127ed9bb
Brett Graham <brettgraham@gmail.com>
2016-11-17 12:57:39 -0500

    add template to example, update c3, add header

--- examples/01/start_ionode.py
+++ examples/01/start_ionode.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+import time
+
 import pizco
+import tornado.ioloop
 
 import ionode.base
 
@@ -10,6 +13,7 @@ class MyIONode(ionode.base.IONode):
         super(MyIONode, self).__init__(cfg)
         self.mysignal1 = pizco.Signal(nargs=1)
         self.mysignal2 = pizco.Signal(nargs=1)
+        self._update_cb = None
         self.counter = 0
 
     def trigger_signal1(self, arg):
@@ -19,6 +23,24 @@ class MyIONode(ionode.base.IONode):
     def trigger_signal2(self, arg):
         print "Triggering signal2 with %s" % (arg, )
         self.mysignal2.emit(arg)
+
+    def connect(self):
+        if self._update_cb is not None:
+            return
+        self._update_cb = tornado.ioloop.PeriodicCallback(
+            self.update, 500, self.loop)
+        self._update_cb.start()
+
+    def disconnect(self):
+        if self._update_cb is None:
+            return
+        self._update_cb.stop()
+        while self._update_cb._running:
+            time.sleep(0.1)
+        self._update_cb = None
+
+    def connected(self):
+        return self._update_cb is not None
 
     def update(self):
         self.mysignal2.emit(self.counter)
--- examples/01/start_ui.py
+++ examples/01/start_ui.py
@@ -6,10 +6,17 @@ import webbrowser
 import ionode.base
 import ionode.ui.base
 
+import pizco
 from start_ionode import MyIONode
+
+spec = {
+    'template': open('template.html', 'r').read()
+}
 
 url = 'http://127.0.0.1:5000/base'
 config = json.load(open('config.json', 'r'))
 print("Use Ctrl-C in this terminal window to stop the ui and server")
 webbrowser.open(url)
-ionode.ui.base.run_proxy_ui('base', config['addr'], MyIONode)
+p = pizco.Proxy(config['addr'])
+ionode.ui.register_proxy(p, 'base', MyIONode, spec)
+ionode.ui.run(debug=True)
